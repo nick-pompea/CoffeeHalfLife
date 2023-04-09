e9d9189a9f39a846b51b4b5bb65650df111c4ae1
Nicholas Pompea <nick.pompea@gmail,com>
2023-04-09 12:35:29 -0600

    Initial Commit

---
+++ CoffeeHalfLife/CoffeeHalfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeHalfLifeApp.swift
+//  CoffeeHalfLife
+//
+//  Created by Nicholas Pompea on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeHalfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
